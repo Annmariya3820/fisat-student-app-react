d45299f3ef5f93d00f43b32a39afefbfe08ce69c
Annmariya3820 <mariyadaisy320@gmail.com>
2024-06-09 16:08:25 +0530

    completed

---
+++ src/components/AddStudent.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react'
+import Navbar from './Navbar'
+
+const AddStudent = () => {
+    const [data,setData]=useState(
+        {
+            "firstname":"",
+            "lastname":"",
+            "college":"",
+            "dob":"",
+            "course":"",
+            "phno":"",
+            "email":"",
+            "address":""
+
+        }
+    )
+    const inputHandler=(event)=>{
+       setData ({...data,[event.target.name]:event.target.value})
+    }
+    const readValue=()=>{
+        console.log(data)
+    }
+  return (
+    <div>
+        <Navbar/>
+      <div className="container">
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div className="row g-3">
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                     <label htmlFor="" className="form-label">First Name</label>
+                     <input type="text" className="form-control" name='firstname' value={data.firstname} onChange={inputHandler}/>
+
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form label">Last Name</label>
+                    <input type="text" className="form-control" name='lastname' value={data.lastname} onChange={inputHandler}/>
+
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                    <label htmlFor="" className="form-label">College</label>
+                    <input type="text" className="form-control"name='college' value={data.college} onChange={inputHandler} />
+
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                    <label htmlFor="" className="form-label">Dob</label>
+                    <input type="date" className="form-control" name='dob'value={data.dob} onChange={inputHandler} />
+
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                    <label htmlFor="" className="form-label">Course</label>
+                    <select name="course" id="" className="form-control" value={data.course} onChange={inputHandler} >
+                        <option value="select option">select option</option>
+                        <option value="MCA">MCA</option>
+                        <option value="MBA">MBA</option>
+                        <option value="Btech-cs">Btech-cs</option>
+                    </select>
+
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+
+                    <label htmlFor="" className="form-label">Ph.No</label>
+                    <input type="number" className="form-control" name='phno' value={data.phno} onChange={inputHandler} />
+
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                     <label htmlFor="" className="form-label">Email</label>
+                     <input type="email" className="form-control" name='email' value={data.email} onChange={inputHandler}/>
+
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                     <label htmlFor="" className="form-label">Address</label>
+                     <textarea name="address" id="" className="form-control" value={data.address} onChange={inputHandler}></textarea>
+
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+
+                   <button className="btn btn-success" onClick={readValue}>Submit</button>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AddStudent
